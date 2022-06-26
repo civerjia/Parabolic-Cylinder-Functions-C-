238496b564cdd9fc83e6667ac9cb2df3a69e86b8
SHUANG <shuangzhou@wustl.edu>
2022-06-26 00:21:53 -0500

    Update and rename verify.m to benchmark.m

--- benchmark.m
+++ benchmark.m
@@ -1,6 +1,7 @@
 a = -1;
 x = linspace(4,5,1e6);
 % val1 = pwlx(a,x);
+t = zeros(5,1);
 for i = 1:5
 tic;
 val2 = pcf_mex(a,x,'pwlx');
@@ -11,3 +12,11 @@ mean(t)
 % 1.0733 vs + openmp + avx2
 % 1.0858 vs + openmp
 % 9.7933 vs
+%%
+tic;
+val1 = zeros(1e6,1);
+parfor i = 1:1e6
+    val1(i) = pwlx(a,x(i));
+end
+toc;
+mse(val1 - val2)
